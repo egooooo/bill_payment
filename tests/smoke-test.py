f8adef016c303b4456d56a4c8eda87277e2e01f9
egoo <golosuev@gmail.com>
2020-11-17 10:26:11 +0200

    fix

--- tests/smoke-test.py
+++ tests/smoke-test.py
@@ -1,16 +1,15 @@
 import logging
-import sys
 import unittest
 
 import requests
-import urllib3
-
 
 
 class SmokeTests(unittest.TestCase):
     def testEndPointExist(self):
         logging.info("Ensuring endpoints exist")
-        self.assertEqual(SmokeTests.get("http://127.0.0.1:5000/").status_code, 200)
+        self.assertEqual(
+            SmokeTests.get("http://127.0.0.1:5000/").status_code, 200
+        )
 
     @staticmethod
     def get(url):
